1cfa9636ec5f796810fea23807c2432d3ad81cdc
hitigon <hitigon@gmail.com>
2014-09-14 14:16:01 -0700

    update question parsing

--- crawler.py
+++ crawler.py
@@ -63,19 +63,7 @@ class Crawler(threading.Thread):
         return url
 
 
-class QuestionObject(object):
-
-    def __init__(self):
-        self.id = 0
-        self.author = None
-        self.title = None
-        self.content = None
-        self.upvotecount = 0
-        self.favoritecount = 0
-        self.viewcount = 0
-        self.tags = []
-        self.comments = []
-        self.time = None
+class DataObject(object):
 
     @classmethod
     def create(self, **kwargs):
@@ -84,14 +72,37 @@ class QuestionObject(object):
         return self
 
 
-class AnswerObject(object):
+class UserObject(DataObject):
 
-    def __init__(self, _id, qid):
-        self.id = _id
-        self.qid = qid
+    def __init__(self):
+        self.id = -1
+        self.username = None
+
+
+class QuestionObject(DataObject):
+
+    def __init__(self):
+        self.id = -1
+        self.author = None
+        self.title = None
+        self.content = None
+        self.upvotecount = 0
+        self.favoritecount = 0
+        self.viewcount = 0
+        self.tags = []
+        self.comments = []
+        self.askedtime = None
+        self.activitytime = None
+
+
+class AnswerObject(DataObject):
+
+    def __init__(self):
+        self.id = -1
+        self.qid = -1
         self.author = None
         self.content = None
-        self.upvotes = 0
+        self.upvotecount = 0
         self.accepted = False
         self.comments = []
         self.time = None
@@ -108,16 +119,46 @@ class StackExchangeHandler(object):
 
     def add_question(self):
         question_tag = self.soup.select('#question')[0]
-        post_tag = self.soup.select('td.postcell div.post-text')[0]
-        qid = question_tag.get('data-questionid')
         title = self.soup.select('a.question-hyperlink')[0].string
+        qid = question_tag.get('data-questionid')
+        post_tag = question_tag.select('div.post-text')[0]
         content = post_tag.contents
+        upvotecount = question_tag.select('span.vote-count-post')[0].string
+        favoritecount = question_tag.select('div.favoritecount b')[0].string
+        owner_tag = question_tag.select('td.owner')[0]
+        user_tag = owner_tag.select('div.user-details')[0]
+        user_link_tag = user_tag.select('a')
+
+        if user_link_tag:
+            tmp = user_link_tag[0].get('href').split('/')
+            uid = tmp[2]
+            username = tmp[3]
+        else:
+            uid = -1
+            username = user_tag.contents[0].strip()
+        author = UserObject.create(id=uid, username=username)
+        info_tag = self.soup.select('#qinfo td p.label-key')
+        askedtime = info_tag[1].get('title')
+        viewcount = info_tag[3].select('b')[0].string.split(' ')[0]
+        if len(info_tag) >= 6:
+            activitytime = info_tag[5].select('a')[0].get('title')
+        else:
+            activitytime = 0
+        if not favoritecount:
+            favoritecount = 0
         tags = [
             tag.string for tag in post_tag.find_next_sibling().find_all('a')]
         question = QuestionObject.create(
-            id=qid, title=title, content=content, tags=tags)
-        logger.info('Added a question:' + question.title)
+            id=qid, title=title, content=content,
+            tags=tags, upvotecount=int(upvotecount),
+            favoritecount=int(favoritecount), author=author,
+            viewcount=viewcount, askedtime=askedtime,
+            activitytime=activitytime)
+        logger.info('Added a question:' + str(question.title))
         return qid
+
+    def add_answer(self, qid):
+        pass
 
 if __name__ == '__main__':
 
