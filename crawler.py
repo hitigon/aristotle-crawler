2238cef0dd9129dcf755d9af1d5c831c085653bf
hitigon <hitigon@gmail.com>
2014-09-15 13:47:55 -0700

    add targeted elements, fixed some bugs

--- crawler.py
+++ crawler.py
@@ -17,10 +17,11 @@ logger = logging.getLogger('Crawler')
 
 class Crawler(threading.Thread):
 
-    def __init__(self, url, target_link=None, depth=2, handler=None):
+    def __init__(self, url, target=None, targets=None, depth=1, handler=None):
         threading.Thread.__init__(self)
         self.url = url
-        self.target_link = target_link
+        self.target = target
+        self.targets = targets
         self.depth = depth
         self.handler = handler
         parsed_url = urlparse(self.url)
@@ -30,29 +31,35 @@ class Crawler(threading.Thread):
 
     def run(self):
         logger.info('Starting...')
-        self.crawler(self.url, self.depth)
+        self.crawler(self.url, 0)
         logger.info('Stopping...')
 
     def crawler(self, url, depth):
+        if depth == self.depth:
+            return
         url = self.unique_url(url)
-        if depth == 0 or url in self.visited:
+        if url in self.visited:
             return
         try:
-            logger.debug('crawling %s' % url)
+            logger.debug('crawling %s at %d' % (url, depth))
             f = urllib2.urlopen(url)
             self.visited.add(url)
             soup = BeautifulSoup(f)
             if self.handler:
                 self.handler(soup, url, depth)
-            if self.target_link:
-                urls = soup.find_all('a', class_=self.target_link)
+            if self.targets and depth in self.targets:
+                urls = soup.select(self.targets[depth])
+            elif self.target:
+                urls = soup.find_all('a', class_=self.target)
             else:
                 urls = soup.find_all('a')
             for u in urls:
                 u = self.fix_url(u.get('href'))
-                self.crawler(u, depth - 1)
+                self.crawler(u, depth + 1)
         except Exception as e:
-            logger.error(e)
+            import sys
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            logger.error('%s at %r' % (str(e), exc_tb.tb_lineno))
             return
 
     def fix_url(self, url):
@@ -81,9 +88,6 @@ class UserObject(DataObject):
         self.id = -1
         self.username = None
 
-    def __repr__(self):
-        return '%s (%r)' % (self.username, self.id)
-
 
 class QuestionObject(DataObject):
 
@@ -99,6 +103,7 @@ class QuestionObject(DataObject):
         self.comments = []
         self.askedtime = None
         self.activitytime = None
+        self.wiki = False
 
 
 class AnswerObject(DataObject):
@@ -112,6 +117,7 @@ class AnswerObject(DataObject):
         self.accepted = False
         self.comments = []
         self.answeredtime = None
+        self.wiki = False
 
 
 class CommentObject(DataObject):
@@ -142,17 +148,30 @@ class StackExchangeHandler(object):
         content = post_tag.contents
         upvotecount = question_tag.select('span.vote-count-post')[0].string
         favoritecount = question_tag.select('div.favoritecount b')[0].string
-        owner_tag = question_tag.select('td.owner')[0]
-        user_tag = owner_tag.select('div.user-details')[0]
-        user_link_tag = user_tag.select('a')
-
-        if user_link_tag:
-            tmp = user_link_tag[0].get('href').split('/')
-            uid = tmp[2]
-            username = tmp[3]
+        owner_tag = question_tag.select('td.owner')
+        wiki = False
+        if owner_tag:
+            user_tag = owner_tag[0].select('div.user-details')[0]
+            user_link_tag = user_tag.select('a')
+            if user_link_tag:
+                tmp = user_link_tag[0].get('href').split('/')
+                uid = tmp[2]
+                username = tmp[3]
+            else:
+                uid = -1
+                username = user_tag.contents[0].strip()
         else:
-            uid = -1
-            username = user_tag.contents[0].strip()
+            wiki = True
+            wiki_tag = question_tag.select('div.user-info')[-1]
+            user_tag = wiki_tag.select('div.user-details')[-1]
+            user_link_tag = user_tag.select('a')
+            if len(user_link_tag) == 2:
+                tmp = user_link_tag[-1].get('href').split('/')
+                uid = tmp[2]
+                username = user_link_tag[-1].string.strip()
+            else:
+                uid = -1
+                username = user_link_tag[0].string.strip()
         author = UserObject.create(id=int(uid), username=username)
         info_tag = self.soup.select('#qinfo td p.label-key')
         askedtime = info_tag[1].get('title')
@@ -173,8 +192,9 @@ class StackExchangeHandler(object):
             tags=tags, upvotecount=int(upvotecount),
             favoritecount=int(favoritecount), author=author,
             viewcount=viewcount, askedtime=askedtime,
-            activitytime=activitytime, comments=comments)
-        logger.info('Added a question: %s' % str(question.title))
+            activitytime=activitytime, comments=comments,
+            wiki=wiki)
+        logger.info('Added a question: %r' % question.title)
         return int(qid)
 
     def add_answer(self, qid):
@@ -189,19 +209,33 @@ class StackExchangeHandler(object):
                 accepted = True
             else:
                 accepted = False
+            # there is a bug for certain posts that cannot find any .user_info
             owner_tag = answer_tag.select('div.user-info')[-1]
-            user_tag = owner_tag.select('div.user-details')[0]
+            user_action_tag = owner_tag.select('div.user-action-time')
+            user_tag = owner_tag.select('div.user-details')[-1]
             user_link_tag = user_tag.select('a')
-            user_action_tag = owner_tag.select('div.user-action-time span')[0]
-            if user_link_tag:
-                tmp = user_link_tag[0].get('href').split('/')
-                uid = tmp[2]
-                username = tmp[3]
+            answeredtime = None
+            wiki = False
+            if user_action_tag:
+                answeredtime = user_action_tag[
+                    -1].select('span')[0].get('title')
+                if user_link_tag:
+                    tmp = user_link_tag[0].get('href').split('/')
+                    uid = tmp[2]
+                    username = tmp[3]
+                else:
+                    uid = -1
+                    username = user_tag.contents[0].strip()
             else:
-                uid = -1
-                username = user_tag.contents[0].strip()
+                wiki = True
+                if len(user_link_tag) == 2:
+                    tmp = user_link_tag[-1].get('href').split('/')
+                    uid = tmp[2]
+                    username = user_link_tag[-1].string.strip()
+                else:
+                    uid = -1
+                    username = user_link_tag[0].string.strip()
             author = UserObject.create(id=int(uid), username=username)
-            answeredtime = user_action_tag.get('title')
             comments_tag = answer_tag.select(
                 'div.comments')[0].select('tr.comment')
             comments = self.add_comment(comments_tag)
@@ -209,7 +243,7 @@ class StackExchangeHandler(object):
                 id=int(aid), qid=qid, accepted=accepted,
                 content=content, upvotecount=int(upvotecount),
                 author=author, answeredtime=answeredtime,
-                comments=comments)
+                comments=comments, wiki=wiki)
             logger.info('Added an answer by: %s' % answer.author.username)
 
     def add_comment(self, comments_tag):
@@ -218,15 +252,15 @@ class StackExchangeHandler(object):
             mid = comment_tag.get('id').split('-')[1]
             comment_body_tag = comment_tag.select('div.comment-body')[0]
             content = comment_body_tag.select('span.comment-copy')[0].contents
-            user_tag = comment_body_tag.select('a.comment-user')[0]
+            user_tag = comment_body_tag.select('a.comment-user')
             if user_tag:
-                tmp = user_tag.get('href').split('/')
+                tmp = user_tag[0].get('href').split('/')
                 uid = tmp[2]
                 username = tmp[3]
             else:
                 uid = -1
                 username = comment_body_tag.select(
-                    'span.comment-user')[0].string
+                    'span.comment-user')[0].string.strip()
             user = UserObject.create(id=int(uid), username=username)
             score_tag = comment_tag.select('td.comment-score span.cool')
             if score_tag:
@@ -246,11 +280,14 @@ class StackExchangeHandler(object):
 if __name__ == '__main__':
 
     urls = [
-        'http://matheducators.stackexchange.com/questions?page=1&sort=newest',
+        'http://math.stackexchange.com/questions?page=1&sort=votes',
     ]
+
+    targets = {0: 'a.question-hyperlink', 1: 'div.pager-answers a'}
 
     for url in urls:
         crawler = Crawler(
-            url, 'question-hyperlink', handler=StackExchangeHandler)
+            url, targets=targets,
+            depth=3, handler=StackExchangeHandler)
         crawler.start()
         crawler.join()
